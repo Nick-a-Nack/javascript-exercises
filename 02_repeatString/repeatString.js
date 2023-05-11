969e2c0a854800de5f94d69d7e005ea718a4f9a4
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-10 18:37:50 -0700

    Completed lesson 2 with 7 out of 7 passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(input, count) {
+    let output = "";
+    if (count < 0){
+        return "ERROR";
+    }
+    else{
+        for (let i = 0; i < count; i++){
+            output = output.concat(input);
+        }
+        return output;
+    }    
 };
 
 // Do not edit below this line
