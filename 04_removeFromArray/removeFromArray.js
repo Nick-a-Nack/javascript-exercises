b971eba8e68ddf6790628beefab5c03afb119974
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-12 00:58:03 -0700

    Completed lesson 4, removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let output = [];
+    for (let i = 0; i < array.length; i++){
+        let found = false;
+        for (let j = 0; j < args.length; j++){
+            if (array[i] === args[j]){
+                found = true;
+                break;
+            }
+        }
+        if (!found){
+            output.push(array[i]);
+        }
+        else{
+            found = false;
+        }
+    }
 
+    return output;
 };
 
 // Do not edit below this line
