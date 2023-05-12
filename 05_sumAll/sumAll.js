0800a698dd344ef3fc72879c2ac3382bd206700c
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-12 01:16:35 -0700

    Completed lesson 5, sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if (typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR";
+    }
+    if (a < 0 || b < 0){
+        return "ERROR";
+    }
+    if (a == b){
+        return a;
+    }
+    else if (a < b){
+        return 1 + ((b * (b + 1)) - (a * (a + 1))) / 2;
+    }
+    else{
+        return 1 + ((a * (a + 1)) - (b * (b + 1))) / 2; 
+    }
 };
 
 // Do not edit below this line
