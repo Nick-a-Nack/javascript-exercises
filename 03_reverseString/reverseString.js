272b11a689382c69a20ff93e34b61582e179391b
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-12 00:57:24 -0700

    Completed reverseString assignment for JS Practice

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let temp = input.split("");
+    let output = "";
 
+    for (let i = 0; i < temp.length; i++){
+        output = output.concat(temp[temp.length - i - 1]);
+    }
+    return output;
 };
 
 // Do not edit below this line
