3e5e493fd1bdd77a7da915fe4a26239a0539b724
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-13 00:59:12 -0700

    Finished fibonacci test for 100th time

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0){
+        return "OOPS";
+    }
+    else{
+        let temp = parseInt(input);
+        if (temp == 1 || temp == 2){
+            return 1;
+        }
+        else{
+            return fibonacci(temp - 1) + fibonacci(temp - 2);
+        }
+    }
 };
 
 // Do not edit below this line
