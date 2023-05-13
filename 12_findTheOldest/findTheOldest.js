efaea9a75a752c5d91ee73c1501962fcd4825938
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-13 01:40:40 -0700

    Finished findOldest function using helper getAge function on each item in obj array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(group) {
+    let temp = group[0];
+    let currentAge = getAge(group[0]);
+    for (let i = 1; i < group.length; i++){
+        if (getAge(group[i]) > currentAge){
+            temp = group[i];
+            currentAge = getAge(group[i]);
+        }
+    }
+    return temp;
+};
 
+const getAge = function(person){
+    if (person.hasOwnProperty('yearOfDeath')){
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    else{
+        let temp = new Date();
+        let currentYear = temp.getFullYear();
+        return currentYear - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
