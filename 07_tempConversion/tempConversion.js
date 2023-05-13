761ff81f20b505324f99e69aa4f578f992be884e
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-13 00:11:00 -0700

    Finished tempConversion lesson for JS Practice

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  let output = (input - 32) * (5 / 9);
+  output = output.toFixed(1);
+  output = parseFloat(output);
+  return output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let output = (input * 9 / 5) + 32;
+  output = output.toFixed(1);
+  output = parseFloat(output);
+  return output;
 };
 
 // Do not edit below this line
