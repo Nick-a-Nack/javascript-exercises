f8e36ece80e0ec0fcce42432e64b797997d0dc25
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-13 00:49:41 -0700

    Finished palindrome test for 100th time

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+// First lowercases string,  remove non alpha numeric characters, 
+// converts to array, tests for palindrome-ness
 
+const palindromes = function (input) {
+    let temp = input.replace(/\W/g, '').toLowerCase();
+    let test = temp.split("");
+    for (let i = 0; i < temp.length / 2; i++){
+        if (test[i] != test[test.length - i - 1]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
