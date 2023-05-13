c8a81345b6d719232f61dc168a858d4a0d241ba8
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-05-12 23:45:39 -0700

    Completed leap year assignment for JS Practice

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(input) {
+    if ((input % 4) == 0){
+        if ((input % 100) == 0){
+            if ((input % 400) == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
